70a68bc3f246db7a88a7a51feeb6e322902b2ecd
Vyas Ramankulangara <vyas01890@outlook.com>
2020-02-27 10:49:32 -0600

    added more attributes

--- db/hospital.sql
+++ db/hospital.sql
@@ -1,19 +1,22 @@
 CREATE TABLE IF NOT EXISTS patient
 (
     patient_id           SERIAL PRIMARY KEY NOT NULL,
-    patient_first_name   varchar(100)       NOT NULL,
-    patient_last_name    varchar(100)       NOT NULL,
+    patient_first_name   VARCHAR(100)       NOT NULL,
+    patient_last_name    VARCHAR(100)       NOT NULL,
     patient_email        TEXT UNIQUE        NOT NULL,
     patient_address      TEXT               NOT NULL,
     patient_phone_number TEXT               NOT NULL,
-    patient_gender       varchar(7)         NOT NULL,
-    patient_dob          date               NOT NULL
+    patient_age          INT                NOT NULL,
+    patient_gender       VARCHAR(7)         NOT NULL,
+    patient_dob          DATE               NOT NULL,
+    patient_username     VARCHAR(100)       NOT NULL,
+    patient_password     VARCHAR(100)       NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS appointment
 (
     appt_id     SERIAL PRIMARY KEY,
-    appt_date   TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
+    appt_date   TIMESTAMP NOT NULL DEFAULT NOW(),
     appt_reason TEXT      NOT NULL
 );
 
@@ -26,7 +29,9 @@ CREATE TABLE IF NOT EXISTS doctor
     doctor_phone_number TEXT         NOT NULL,
 --   doctor_office REFERENCES office(office_id),     --Refrence to office regarding place of work/employment
     doctor_spec         TEXT         NOT NULL,
-    doctor_availability TSTZRANGE[]  NOT NULL
+    doctor_availability TSTZRANGE[]  NOT NULL,
+    doctor_username     VARCHAR(100) NOT NULL,
+    doctor_password     VARCHAR(100) NOT NULL
     -- patients INT[] NOT NULL
     --open to a table or list of patient IDs (health_record_id)      --Refrence to Patient regarding care
     --Refrence to diagnosis regarding decision of treatment?
@@ -35,20 +40,20 @@ CREATE TABLE IF NOT EXISTS doctor
 CREATE TABLE IF NOT EXISTS office
 (
     office_id           SERIAL PRIMARY KEY,
-    office_capacity     INT       NOT NULL,
-    office_phone_number TEXT      NOT NULL,
-    office_opening_hour time NOT NULL,
-    office_address      TEXT      NOT NULL,
-    office_specialty    TEXT      NOT NULL
+    office_capacity     INT  NOT NULL,
+    office_phone_number TEXT NOT NULL,
+    office_opening_hour TIME NOT NULL,
+    office_address      TEXT NOT NULL,
+    office_specialty    TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS test
 (
     test_id        SERIAL PRIMARY KEY,
-    test_date      TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
-    test_scan      boolean   NOT NULL,
-    test_physical  boolean   NOT NULL,
-    test_blood     boolean   NOT NULL,
+    test_date      TIMESTAMP NOT NULL DEFAULT NOW(),
+    test_scan      BOOLEAN   NOT NULL,
+    test_physical  BOOLEAN   NOT NULL,
+    test_blood     BOOLEAN   NOT NULL,
     test_equipment TEXT      NOT NULL
     -- test_office REFERENCES office(office_id),
     -- test_doctor REFERENCES doctor(doctor_id)
@@ -63,9 +68,9 @@ CREATE TABLE IF NOT EXISTS diagnosis
 
 CREATE TABLE "session"
 (
-    "sid"    varchar      NOT NULL COLLATE "default",
-    "sess"   json         NOT NULL,
-    "expire" timestamp(6) NOT NULL
+    "sid"    VARCHAR      NOT NULL COLLATE "default",
+    "sess"   JSON         NOT NULL,
+    "expire" TIMESTAMP(6) NOT NULL
 )
     WITH (OIDS= FALSE);
 ALTER TABLE "session"
