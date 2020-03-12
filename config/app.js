9eda6c392e95a6290bd6551c724b18bcddde60bd
Vyas Ramankulangara <vyas01890@outlook.com>
2020-03-11 23:39:52 -0500

    Revert "expirmenting with reterving doctor time data, added moment.js"
    This reverts commit ec2f959a35c147a0f4c6066631c856b66e4a33b0.

--- config/app.js
+++ config/app.js
@@ -55,10 +55,10 @@ app.get('/', (req, res) => {
   res.redirect('/api');
 });
 
-// app.get('/test', (req, res) => {
-//   req.session.user = { user: 'username' };
-//   res.send({ user: 'username' });
-// });
+app.get('/test', (req, res) => {
+  req.session.user = { user: 'username' };
+  res.send({ user: 'username' });
+});
 
 app.get('/api', (req, res) => {
   res.status(200).json({ msg: 'Welcome to the Medical Clinic API!' });
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,21 @@
 const { Router } = require('express');
-const moment = require('moment');
 const db = require('../config/db');
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const { rows } = await db.query({ text: 'SELECT (doctor_availability) FROM doctor;' });
-    if (rows.length > 0) {
-      const t = rows[0].doctor_availability.map((d, i) => {
-        if (!d.taken) {
-          const tem = d.time.split(',')[0].replace('[', '');
-          const date = moment(new Date(Date.parse(tem))).utc().format('MM/DD/YYYY');
-          const time = moment(new Date(Date.parse(tem))).utc().format('hh:mm:ss A');
-          return {
-            idx: i + 1, date, time, taken: d.taken,
-          };
-        }
-      });
+router.post('/', async (req, res) => {
+  const { id } = req.body;
 
-      res.status(200).json({ t });
+  try {
+    const { rows } = await db.query({ text: 'SELECT * FROM users WHERE id = $1', values: [id] });
+    if (rows.length > 0) {
+      res.status(200).json(rows);
     } else {
       res.status(500).json({ message: 'User Not Found' });
     }
   } catch (err) {
     // console.error(err);
-    res.json({ message: 'Please enter a valid ID', err });
+    res.json({ message: 'Please enter a valid ID' });
   }
   // res.send('Hello');
 });
