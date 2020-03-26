426a1cded248935bdba8821a93b60cf301deff65
Michael Austin <mpaustin1993@gmail.com>
2020-03-26 14:30:01 -0500

    idk

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,30 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true,
-		node: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'airbnb',
-	],
-	globals: {
-		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly',
-	},
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 2018,
-	},
-	plugins: [
-		'react',
-	],
-	rules: {
-		"no-console": "off",
-		"linebreak-style": "off",
-		"consistent-return": "off",
-		"indent": 0,
-		"import/no-cycle": "off",
-		"func-names": "off",
-		"no-use-before-define": "off",
-		"import/prefer-default-export": "off",
-		"max-len": "off",
-		"no-tabs": "off"
-	},
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "no-console": "off",
+    "linebreak-style": "off",
+    "consistent-return": "off",
+  },
 };
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,35 @@
 const { Router } = require('express');
-// const moment = require('moment');
-// const db = require('../config/db');
+const moment = require('moment');
+const db = require('../config/db');
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-	// try {
-	// 	const { rows } = await db.query({ text: 'SELECT (doctor_availability) FROM doctor;' });
-	// 	if (rows.length > 0) {
-	// 		const t = rows[0].doctor_availability.map((d, i) => {
-	// 			if (!d.taken) {
-	// 				const tem = d.time.split(',')[0].replace('[', '');
-	// 				const date = moment(new Date(Date.parse(tem))).utc().format('MM/DD/YYYY');
-	// 				const time = moment(new Date(Date.parse(tem))).utc().format('hh:mm:ss A');
-	// 				return {
-	// 					idx: i + 1, date, time, taken: d.taken,
-	// 				};
-	// 			}
-	// 		});
+  try {
+    const { rows } = await db.query({ text: 'SELECT (doctor_availability) FROM doctor;' });
+    if (rows.length > 0) {
+      const t = rows[0].doctor_availability.map((d, i) => {
+        if (!d.taken) {
+          const tem = d.time.split(',')[0].replace('[', '');
+          const date = moment(new Date(Date.parse(tem))).utc().format('MM/DD/YYYY');
+          const time = moment(new Date(Date.parse(tem))).utc().format('hh:mm:ss A');
+          return {
+            idx: i + 1, date, time, taken: d.taken,
+          };
+        }
+      });
 
-	// 		res.status(200).json({ t });
-	// 	} else {
-	// 		res.status(500).json({ message: 'User Not Found' });
-	// 	}
-	// } catch (err) {
-	// 	res.json({ message: 'Please enter a valid ID', err });
-	// }
-	res.send('Hello World');
+      res.status(200).json({ t });
+    } else {
+      res.status(500).json({ message: 'User Not Found' });
+    }
+  } catch (err) {
+        res.json({ message: 'Please enter a valid ID', err });
+  }  
 });
 
 router.get('/test', (req, res) => {
-	res.send("Hey, how's it going");
+  res.send("Hey, how's it going")
 });
 
 module.exports = router;
