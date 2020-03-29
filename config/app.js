d0977a20456835b2b2fcde7551b858821c09c3e2
Vyas Ramankulangara <vyas01890@outlook.com>
2020-03-29 15:00:35 -0500

    added auth route

--- config/app.js
+++ config/app.js
@@ -9,7 +9,7 @@ const { join } = require('path');
 // const session = require('express-session');
 // const PGSession = require('connect-pg-simple')(session);
 const db = require('./db');
-const user = require('../routes/users');
+const auth = require('../routes/auth');
 
 db.connect()
   .then(() => { console.log('Database connected!'); })
@@ -42,7 +42,7 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => {
   res.redirect('/api');
 });
-app.use('/user', user);
+app.use('/auth', auth);
 
 if (isProduction) {
   app.use(express.static(join(__dirname, '../client/build')));
