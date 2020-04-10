765fea9dbcfa89bc53446ad40ec8d074c314f44d
Justin <jswest137@gmail.com>
2020-04-10 13:40:50 -0500

    committed

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Home.css';
+import logo from './logo.png';
+
+console.log(logo); //logo192.png
 
 function Home() {
+
+    const homestyle = {
+        background: 'red', color: 'white', width: '110px'
+    }
+
     return (
-        <div>
-            Home
+        <div className ="home" >
+            <img src={logo} alt="Logo" />;
+            <logo192> </logo192>
+            <h1 style = {homestyle}>Home</h1>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <h1>About Us:</h1>
         </div>
     )
 }
