b0d4270addd270e8856b1411ad7a6ec10f7be067
Michael Austin <mpaustin1993@gmail.com>
2020-04-11 17:44:09 -0500

    stylistic changes to frontend

--- client/src/components/Auth/Doctor/Login/index.js
+++ client/src/components/Auth/Doctor/Login/index.js
@@ -1,5 +1,8 @@
 import { useStoreActions } from 'easy-peasy';
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
 
 const DoctorLoginComponent = () => {
 
@@ -24,36 +27,67 @@ const DoctorLoginComponent = () => {
     };
 
     return (
-        <Fragment>
-            <h1 className='large text-primary'>Doctor Sign In</h1>
-            <p className='lead'>
-                <i className='fas fa-user' /> Sign Into Your Account
-      </p>
-            <form className='form' onSubmit={e => onSubmit(e)}>
-                <div className='form-group'>
-                    <input
-                        type='text'
-                        placeholder='Username'
-                        name='username'
-                        value={username}
-                        onChange={e => onChange(e)}
-                        required
-                    />
-                </div>
-                <div className='form-group'>
-                    <input
-                        type='password'
-                        placeholder='Password'
-                        name='password'
-                        value={password}
-                        onChange={e => onChange(e)}
-                        minLength='6'
-                    />
-                </div>
-                <input type='submit' className='btn btn-primary' value='Login' />
-            </form>
-        </Fragment>
-    )
+		<Fragment>
+			<div className="container-fluid">
+				<div className="row no-gutter">
+					<div className="d-none d-md-flex col-md-4 col-lg-6 bg-doctor-image"></div>
+					<div className="col-md-8 col-lg-6">
+						<div className="login d-flex align-items-center py-5">
+							<div className="container">
+								<div className="row">
+									<div className="col-md-9 col-lg-8 mx-auto">
+										<h3 className="login-heading  mb-4">Log In</h3>
+										<form onSubmit={(e) => onSubmit(e)}>
+											<div className="form-label-group">
+												<input
+													type="text"
+													id="inputUsername"
+													className="form-control"
+													placeholder="Username"
+													name="username"
+													value={username}
+													onChange={(e) => onChange(e)}
+													required
+													autoFocus
+												/>
+												<label htmlFor="inputUsername">
+													Username
+												</label>
+											</div>
+
+											<div className="form-label-group">
+												<input
+													type="password"
+													id="inputPassword"
+													className="form-control"
+													placeholder="Password"
+													name="password"
+													value={password}
+													onChange={(e) => onChange(e)}
+													minLength="6"
+													required
+												/>
+												<label htmlFor="inputPassword">
+													Password
+												</label>
+											</div>
+
+											<button
+												className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2"
+												type="submit"
+											>
+												Sign in
+											</button>										
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 }
 
 export default DoctorLoginComponent
