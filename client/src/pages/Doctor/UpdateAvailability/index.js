16b266b553b8d2c0fef7c5f9faf0555c8495ae24
Justin <jswest137@gmail.com>
2020-04-18 15:51:14 -0500

    Add and update availability for doctor

--- client/src/pages/Doctor/UpdateAvailability/index.js
+++ client/src/pages/Doctor/UpdateAvailability/index.js
@@ -1,11 +1,13 @@
 import { useStoreActions, useStoreState } from 'easy-peasy';
+import moment from 'moment';
 import React, { useEffect } from 'react';
 import { Table } from 'react-bootstrap';
+//import Loading from 'src/components/Loading';
 import './UpdateAvailability.css';
 
 const UpdateAvailability = () => {
 
-    const availability = useStoreState(state => state.doctor.availability);
+    const availability = useStoreState(state => state.doctor.allAvailability);
     // const updateAvailability = useStoreActions(actions => actions.doctor.updateAvailability);
     const getAllAvailability = useStoreActions(actions => actions.doctor.getAllAvailability);
     const getOffices = useStoreActions((actions) => actions.doctor.getOffices);
@@ -34,16 +36,20 @@ const UpdateAvailability = () => {
         </thead>
         <tbody>
             {
-                loading ? <Loading/> :
+                //loading ? <Loading/> :
                 
-                // availability.map((availability, idx) => {
-                //     return (
-                //         <tr key={idx}>
-                //             <td>{`${availability.address_name} ${availability.address2_name ? availability.address2_name : ''}, ${availability.city} ${availability.state} ${availability.zip}`}</td>
-                //             <td>{moment(availability.availability_date).format('MM/DD/YYYY')}</td>
-                //         </tr>
-                //     );
-                // })
+                availability.map((availability, idx) => {
+                    return (
+                        <tr key={idx}>
+                            <td>{`${availability.address_name} ${availability.address2_name ? availability.address2_name : ''}, ${availability.city} ${availability.state} ${availability.zip}`}</td>
+                            <td>{moment(availability.availability_date).format('MM/DD/YYYY')}</td>
+                            <td>
+                                    <p className="cancelApp badge badge-danger" onClick={(e) => {
+                                    }}>Update</p>
+                                </td>
+                        </tr>
+                    );
+                })
             }
         </tbody>
     </Table >
