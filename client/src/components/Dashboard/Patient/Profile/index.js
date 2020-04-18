ce644a6ee2bad29b729e12a6ce55683fa09233ad
anhnguyen <mr.quocanh191997@gmail.com>
2020-04-18 18:24:40 -0500

    Patient Profile

--- client/src/components/Dashboard/Patient/Profile/index.js
+++ client/src/components/Dashboard/Patient/Profile/index.js
@@ -70,7 +70,15 @@ const PatientProfile = () => {
 
     return (
         <>
-            <div className="profileContainer">
+        <div className="container-fluid">
+			<div className="row no-gutter">
+				<div className="d-none d-md-flex col-md-4 col-lg-6 bg-patient-dashboard"></div>
+				<div className="col-md-8 col-lg-6">
+					<div className="login d-flex align-items-center py-5">
+						<div className="container">
+							<div className="row">
+								<div className="col-md-9 col-lg-8 mx-auto">
+                                <div className="profileContainer">
                 {loading ? <Loading /> :
                     (
                         <Card>
@@ -295,7 +303,16 @@ const PatientProfile = () => {
                     </Modal.Body>
                 </Modal>
             </>
+									<div className="body text-center"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
         </>
+         
     )
 }
 
